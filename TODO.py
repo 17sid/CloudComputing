d8ee97aaf48a1ebf79d56bfd6fa172c45bd14534
17sid <73870520+17sid@users.noreply.github.com>
2022-05-04 15:25:57 +0530

    Update TODO.py

--- TODO.py
+++ TODO.py
@@ -1,5 +1,21 @@
 # Importing all the necessary modules
 from tkinter import *
+def add_item(entry: Entry, listbox: Listbox):
+    new_task = entry.get()
+    listbox.insert(END, new_task)
+    with open('tasks.txt', 'a') as tasks_list_file:
+        tasks_list_file.write(f'\n{new_task}')
+
+def delete_item(listbox: Listbox):
+    listbox.delete(ACTIVE)
+    with open('tasks.txt', 'r+') as tasks_list_file:
+        lines = tasks_list_file.readlines()
+        tasks_list_file.truncate()
+        for line in lines:
+            if listbox.get(ACTIVE) == line[:-2]:
+                lines.remove(line)
+            tasks_list_file.write(line)
+        tasks_list_file.close()
 # Initializing the python to do list GUI window
 root = Tk()
 root.title('TechVidvan To-Do List')
@@ -33,21 +49,4 @@ delete_btn.place(x=150, y=350)
 root.update()
 root.mainloop()
 # Adding and Deleting items functions
-'''
-def add_item(entry: Entry, listbox: Listbox):
-    new_task = entry.get()
-    listbox.insert(END, new_task)
-    with open('tasks.txt', 'a') as tasks_list_file:
-        tasks_list_file.write(f'\n{new_task}')
 
-def delete_item(listbox: Listbox):
-    listbox.delete(ACTIVE)
-    with open('tasks.txt', 'r+') as tasks_list_file:
-        lines = tasks_list_file.readlines()
-        tasks_list_file.truncate()
-        for line in lines:
-            if listbox.get(ACTIVE) == line[:-2]:
-                lines.remove(line)
-            tasks_list_file.write(line)
-        tasks_list_file.close()
-        '''
